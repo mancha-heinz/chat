f92c987c3660db7c072f884b426953ebaa753408
mancha-heinz <manchaheinz@gmail.com>
2023-11-19 17:18:22 -0300

    backend

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const { WebSocketServer } = require("ws");
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error()); // inf. erro
+    ws.on("message", (data) => { // inf. msg
+        // recebe msg e envia p/ todos os clientes q estão no serv
+        wss.clients.forEach((client) => {
+            client.send(data.toString)
+        })
+    })
+
+    console.log("client connected")
+})
